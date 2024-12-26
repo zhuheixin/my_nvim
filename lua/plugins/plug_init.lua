5c50852e6ad97b3f9d90246b5f3d4f35a8827704
Your Name <you@example.com>
2024-12-26 17:27:46 +0800

    add: nvim-gdb

--- lua/plugins/plug_init.lua
+++ lua/plugins/plug_init.lua
@@ -45,6 +45,7 @@ local plugins = {
   "hrsh7th/cmp-nvim-lsp",
   "L3MON4D3/LuaSnip", -- snippets引擎，不装这个自动补全会出问题
   "hrsh7th/cmp-path", -- 文件路径
+  "sakhnik/nvim-gdb"
 }
 local opts = {} -- 注意要定义这个变量
 
