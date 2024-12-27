292c26e62167e6fab1c1c4dddd0750b14c9a0eeb
Your Name <you@example.com>
2024-12-27 21:14:22 +0800

    merge

--- lua/plugins/plug_init.lua
+++ lua/plugins/plug_init.lua
@@ -45,12 +45,9 @@ local plugins = {
   "hrsh7th/cmp-nvim-lsp",
   "L3MON4D3/LuaSnip", -- snippets引擎，不装这个自动补全会出问题
   "hrsh7th/cmp-path", -- 文件路径
-<<<<<<< HEAD
-  "sakhnik/nvim-gdb"
-=======
+  -- "sakhnik/nvim-gdb"
   "hrsh7th/cmp-buffer",     -- 缓冲区补全
   "hrsh7th/cmp-cmdline",}    -- 命令行补全
->>>>>>> origin/mason
 }
 
 local opts = {} -- 注意要定义这个变量
