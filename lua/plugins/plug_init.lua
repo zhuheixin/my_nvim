6e1cb4de26b7127c19024352b862925976f6129d
Your Name <you@example.com>
2025-01-09 18:03:54 +0800

    add:flash.nvim

--- lua/plugins/plug_init.lua
+++ lua/plugins/plug_init.lua
@@ -49,6 +49,20 @@ local plugins = {
   "hrsh7th/cmp-cmdline",},    -- 命令行补全
   -- gdb 
   {"sakhnik/nvim-gdb"},
+  -- flash.nvim 配置
+  {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {   
+    },
+
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+},
 }
 
 local opts = {} -- 注意要定义这个变量
